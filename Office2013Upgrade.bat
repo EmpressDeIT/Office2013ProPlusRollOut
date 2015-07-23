7ea9b3ecfca8c38ab88a8f91a6116566a4f12512
= <=>
2015-07-23 14:37:37 -0700

    Add loop break
    Lines added to stop script from repeating.

--- Office2013Upgrade.bat
+++ Office2013Upgrade.bat
@@ -1,5 +1,13 @@
 @echo off
 
+cd %systemDrive%\users
+dir /s C0mpl3te_0ff1c3.txt
+if errorlevel 0 goto end
+
+echo Started Upgrade > %systemDrive%\users\StartUpgrade.txt
+
+@echo off
+
 wmic product where "description='Microsoft Lync 2010' " uninstall
 
 echo Legacy Lync Uninstall Finished > %systemDrive%\users\Lync_In.txt
@@ -15,5 +23,6 @@ echo Legacy Office Uninstall Finished > %systemDrive%\users\0551c3_1n.txt
 \\dc01\netlogon\setup2013.exe /configure \\dc01\netlogon\configuration2013.xml
 
 echo Office 2013 ProPlus Finished > %systemDrive%\Users\C0mpl3te_0ff1c3.txt
+
 
 Exit
